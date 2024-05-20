4801856fdd7460a6ae02f79ff9f6f825691a45e1
jt <jaterli@hotmail.com>
2024-05-20 23:15:38 +0200

    Creación de auth y usersRouters

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ import morgan from 'morgan';
 
 const app = express();
 
+const DB_URL =
+  process.env.NODE_ENV === "test"
+    ? "mongodb://localhost:27017/ticketing-db-test"
+    : process.env.DB_URL || "mongodb://localhost:27017/ticketing-db";
+
+mongoose
+    .connect(DB_URL)
+    .then(() => console.log(`Connected to DB: ${DB_URL}`))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
+
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get("/", (req, res) => { 
-    res.status(200).send("Hello Word!");
+app.get("/hola", (req, res) => { 
+    res.status(200).send("aquí estoy!");
 });
+
+app.use("/api/users", usersRoutes);
 
 export default app;
